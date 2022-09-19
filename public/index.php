7250cc5f60a291a0ff8c45eb9899b96c8cf1aab7
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-09-19 14:16:27 +0200

    Fixing links

--- public/index.php
+++ public/index.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - Cogip</title>
-    <link rel="stylesheet" href="style/style.css">
-    <script defer src="js/main.js"></script>
-</head>
-<body>
-     <!-- Menu -->
-     <?php require ('view/partials/header.php');?>
-    <!-- End Menu -->
-    <main>
-        <!-- Content -->
-        
-        <!-- End Content -->
+<?php
 
-        <!-- Footer -->
-        <?php require ('view/partials/footer.php');?>
-        <!-- End Footer -->
-    </main>
-</body>
-</html>
+require_once __DIR__.'/../index.php';
