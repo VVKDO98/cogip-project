0d90b73dd2afb0db3bc30cb39c1aeed74815f7a3
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-09-19 16:07:00 +0200

    Draft footer

--- Resources/views/partials/footer.php
+++ Resources/views/partials/footer.php
@@ -8,14 +8,25 @@
         </div>
         <div class="footer__right">
             <div class="footer__infos">
-                <img src="assets/img/address.png" alt="">
-                <p class="footer__address">Square des Martyrs, 6000 Charleroi</p>
-                <img src="assets/img/phone.png" alt="">
-                <p class="footer__phone">(123) 456-7890</p>
-                <img src="assets/img/fax.png" alt="">
-                <p class="footer__fax">(123) 456-7890</p>
+                <div class="footer__address">
+                    <div class="footer__content">
+                        <img src="assets/img/address.png" alt="">
+                        <p>Square des Martyrs, 6000 Charleroi</p>
+                    </div>
+                </div>
+                <div class="footer__contact">
+                    <div class="footer__content">
+                        <img src="assets/img/phone.png" alt="">
+                        <p>(123) 456-7890</p>
+                    </div>
+                    <div class="footer__content">
+                        <img src="assets/img/fax.png" alt="">
+                        <p>(123) 456-7890</p>
+                    </div>
+                </div>
             </div>
             <div class="footer__social">
+                <p>Social Media</p>
                 <img src="assets/img/facebook.png" alt="" class="footer__sociallinks">
                 <img src="assets/img/twitter.png" alt="" class="footer__sociallinks">
                 <img src="assets/img/linkedin.png" alt="" class="footer__sociallinks">
@@ -28,13 +39,19 @@
         </div>
     </div>
     <div class="footer__bottom">
-        <div class="footer__menu">
-            <ul class="footer__list">
-                <li class="footer__item">Home</li>
-                <li class="footer__item">Invoices</li>
-                <li class="footer__item">Compagnies</li>
-                <li class="footer__item">Contacts</li>
-            </ul>
+        <hr class="footer__line">
+        <div class="footer__end">
+            <div class="footer__menu">
+                <ul class="footer__list">
+                    <li class="footer__item">Home</li>
+                    <li class="footer__item">Invoices</li>
+                    <li class="footer__item">Compagnies</li>
+                    <li class="footer__item">Contacts</li>
+                </ul>
+            </div>
+            <div class="footer__copyright">
+                <p>Copyright © 2022 • COGIP Inc.</p>
+            </div>
         </div>
     </div>
 </footer>
