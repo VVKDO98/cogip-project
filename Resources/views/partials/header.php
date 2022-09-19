bdcb2e6106c80f72100c455affd4cb21ffe5aef4
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-09-19 15:16:25 +0200

    Draft header

--- Resources/views/partials/header.php
+++ Resources/views/partials/header.php
@@ -1,5 +1,13 @@
 <header>
     <div class="header__content">
         <?php require ('menu.php');?>
+        <div class="header__cta">
+            <div class="cta__title">
+                <h2>MANAGE YOUR CUSTOMERS AND INVOICES EASLY</h2>
+            </div>
+            <div class="cta__img">
+                <img src="assets/img/men.png" alt="men with laptop">
+            </div>
+        </div>
     </div>
 </header>
