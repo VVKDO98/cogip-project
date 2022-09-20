0a9d2a4f44d3827787aa71fad5f00296a30cc7c9
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-09-20 10:27:05 +0200

    Fix header

--- Resources/views/partials/headerCTA.php
+++ Resources/views/partials/headerCTA.php
@@ -3,7 +3,7 @@
         <?php require ('menu.php');?>
         <div class="header__cta">
             <div class="cta__title">
-                <h2>MANAGE YOUR CUSTOMERS AND INVOICES EASLY</h2>
+                <h2>MANAGE YOUR CUSTOMERS AND INVOICES EASILY</h2>
             </div>
             <div class="cta__img">
                 <img src="assets/img/men.png" alt="men with laptop">
