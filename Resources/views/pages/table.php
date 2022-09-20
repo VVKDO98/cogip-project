4e162f6aeebd8a92409273231320505ff3b54be0
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-09-20 09:38:06 +0200

    route

---
+++ Resources/views/pages/table.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - Cogip</title>
+    <link rel="stylesheet" href="style/style.css">
+    <script defer src="js/main.js"></script>
+</head>
+<body>
+     <!-- Menu -->
+     <?php require ('../resources/views/partials/headerCTA.php');?>
+    <!-- End Menu -->
+    <main>
+        <!-- Content -->
+        
+        <!-- End Content -->
+
+        <!-- Footer -->
+        <?php require ('../resources/views/partials/footer.php');?>
+        <!-- End Footer -->
+    </main>
+</body>
+</html>
