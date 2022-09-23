893e476bbbd2474a8eaa805917ea6fb2ca7b4cb1
EddyVer <vervoorteddy@outlook.com>
2022-09-23 11:53:58 +0200

    test commit phpstorm

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -10,6 +10,7 @@ class LoginController extends Controller
     public function index(){
         return $this->view('login');
     }
+
     public function login($email,$password){
         $datauser = (new LoginModel)->getCredential($email);
 //        echo $datauser[0]["password"] === $password?"true":"false";
