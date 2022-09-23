146b360aaa94763259ed6d4fe2539c105f906e64
EddyVer <vervoorteddy@outlook.com>
2022-09-23 14:44:02 +0200

    modif route form add

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -10,9 +10,6 @@ class LoginController extends Controller
     public function index(){
         return $this->view('login');
     }
-
-
-
     public function login($email,$password){
         $datauser = (new LoginModel)->getCredential($email);
 //        echo $datauser[0]["password"] === $password?"true":"false";
