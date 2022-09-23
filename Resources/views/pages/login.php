4e41956a0a524fc53a261c22557fe301e0658800
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-09-23 11:56:42 +0200

    Login form style

--- Resources/views/pages/login.php
+++ Resources/views/pages/login.php
@@ -4,27 +4,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="style/style.css">
+    <title>Login | Cogip Enterprise</title>
 </head>
-<body>
-    <main>
-        <h1>LOGIN</h1>
-        <div>
-            <form action="/login" method="post">
-                <div>
-                    <label for="email">Email</label>
-                    <input type="email" name="email">
-                </div>
-                <div>
-                    <label for="password">Password</label>
-                    <input type="password" name="password">
-                </div>
-                <button type="submit">login</button>
-            </form>
-        </div>
+<body class="login__body">
+    <main class="login__main">
+        <h1>Login</h1>
+        <form action="/login" method="post" class="login__form">
+            <input type="email" name="email" class="login__input" placeholder="E-mail">
+            <input type="password" name="password" class="login__input" placeholder="Password">
+            <button type="submit" id="login__submit">Login</button>
+        </form>
     </main>
 </body>
 </html>
