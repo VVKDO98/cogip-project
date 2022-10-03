f184850700ea41858e51bbf8cb3154eb86ab4792
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-10-03 16:47:56 +0200

    Burger menu css

--- Resources/views/partials/menu.php
+++ Resources/views/partials/menu.php
@@ -1,24 +1,16 @@
 <nav>
-    <div class="nav__left">
-        <img src="/assets/img/logoClean.png" alt="Logo Cogip" class="nav__logo">
+    <img class="nav__logo" src="/assets/img/logoClean.png" alt="Logo COGIP"/>
+    <div class="nav__menu hideBut">
+        <ul class="nav__list">
+            <li><a href="/" class="nav__item">Home</a></li>
+            <li><a href="/invoices" class="nav__item">Invoices</a></li>
+            <li><a href="/companies" class="nav__item">Companies</a></li>
+            <li><a href="/contacts" class="nav__item">Contacts</a></li>
+        </ul>
+        <ul class="nav__login">
+            <li><a href="#" class="nav__item nav__item--signup">Sign up</a></li>
+            <li><a href="/login" class="nav__item">Login</a></li>
+        </ul>
     </div>
-    <div class="nav__desktop" id="desktopMenu">
-        <div class="nav__middle">
-            <ul class="nav__list">
-                <li><a href="/" class="nav__item">Home</a></li>
-                <li><a href="/invoices" class="nav__item">Invoices</a></li>
-                <li><a href="/companies" class="nav__item">Companies</a></li>
-                <li><a href="/contacts" class="nav__item">Contacts</a></li>
-            </ul>
-        </div>
-        <div class="nav__right">
-            <ul class="nav__login">
-                <li><a href="#" class="nav__item nav__item--signup">Sign up</a></li>
-                <li><a href="/login" class="nav__item">Login</a></li>
-            </ul>
-        </div>
-    </div>
-    <div class="nav__burger">
-        <img src="/assets/img/burgerMenu.png" alt="" id="burgerMenu">
-    </div>
+    <img src="assets/img/burgerMenu.png" alt="" id="nav__burger">
 </nav>
