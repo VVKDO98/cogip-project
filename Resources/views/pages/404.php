97c329dccb79a8e06b5153339fe2dcfca73ab453
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-10-03 14:48:13 +0200

    Change title

--- Resources/views/pages/404.php
+++ Resources/views/pages/404.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style/style.css">
-    <title>404 not found</title>
+    <title>404 | Cogip Enterprise</title>
 </head>
 <body class="four0four__body">
     <main class="four0four__main">
