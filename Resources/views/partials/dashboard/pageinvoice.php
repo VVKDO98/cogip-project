419ca45a5e6fd3ca4a6ab57e9677862bf64af07e
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-10-07 14:17:48 +0200

    Update invoice edition form

--- Resources/views/partials/dashboard/pageinvoice.php
+++ Resources/views/partials/dashboard/pageinvoice.php
@@ -1,33 +1,27 @@
-
-<form method="post" action="/update/invoice">
-    <input type="hidden" name="id" value="<?= $data['invoice'][0]->id  ?>">
-    <div>
-        <label for="ref">Ref</label>
-        <input type="text" name="ref" id="ref" value="<?= $data['invoice'][0]->Ref ?>"  data-default="<?= $data['invoice'][0]->Ref ?>" disabled>
-    </div>
-    <div>
-        <label for="duedates">Due dates</label>
-        <input type="date" name="duedates" id="duedates" value="<?= date("Y-m-d", strtotime($data['invoice'][0]->{'Due dates'})) ?>" data-default="<?= date("Y-m-d", strtotime($data['invoice'][0]->{'Due dates'})) ?>" disabled>
-    </div>
-    <div>
-        <label for="company">Company</label>
-        <select name="company" id="company" disabled data-default="<?= $data['invoice'][0]->id_company ?>">
-        <?php
-            foreach ($data['companies']['datas'] as $item):
-        ?>
-            <option value="<?= $item->id ?>" <?=  $data['invoice'][0]->id_company==$item->id? 'selected="selected"':''; ?> ><?= $item->Name ?></option>
-        <?php endforeach; ?>
+<div class="dashform__main">
+    <div id="tabs">
+    <form method="post" action="/update/invoice" class="dashform__form">
+        <input type="hidden" name="id" value="<?= $data['invoice'][0]->id  ?>">
+        <label for="ref" class="dashform__label">Reference</label>
+        <input type="text" name="ref" id="ref" class="dashform__input" <?= $data['invoice'][0]->Ref ?>"  data-default="<?= $data['invoice'][0]->Ref ?>" disabled>
+        <label for="duedates" class="dashform__label">Due dates</label>
+        <input type="date" name="duedates" id="duedates" class="dashform__input" value="<?= date("Y-m-d", strtotime($data['invoice'][0]->{'Due dates'})) ?>" data-default="<?= date("Y-m-d", strtotime($data['invoice'][0]->{'Due dates'})) ?>" disabled>
+        <label for="company" class="dashform__label">Company</label>
+        <select name="company" id="company" class="dashform__select" disabled data-default="<?= $data['invoice'][0]->id_company ?>">
+            <?php
+                foreach ($data['companies']['datas'] as $item):
+            ?>
+                    <option value="<?= $item->id ?>" <?=  $data['invoice'][0]->id_company==$item->id? 'selected="selected"':''; ?> ><?= $item->Name ?></option>
+            <?php endforeach; ?>
         </select>
+        <label for="price" class="dashform__label">Price</label>
+        <input type="number" step="0.01" name="price" id="price" class="dashform__input" data-default="<?= $data['invoice'][0]->price ?>" value="<?= $data['invoice'][0]->price ?>" disabled>
+        <button type="submit" id="update" class="dashform__submit" style="display: none">Update</button>
+        <button type="button" id="cancel" class="dashform__submit" style="display: none">Cancel</button>
+        <button type="button" id="edit" class="dashform__submit">Edit</button>
+    </form>
     </div>
-    <div>
-        <label for="price">Price</label>
-        <input type="number" step="0.01" name="price" id="price" data-default="<?= $data['invoice'][0]->price ?>" value="<?= $data['invoice'][0]->price ?>" disabled>
-    </div>
-
-    <button type="submit" id="update" style="display: none">Update</button>
-    <button type="button" id="cancel" style="display: none">Cancel</button>
-    <button type="button" id="edit">Edit</button>
-</form>
+</div>
 <script>
     const update = document.querySelector('#update')
     const cancel = document.querySelector('#cancel')
