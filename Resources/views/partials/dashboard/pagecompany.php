275fb6599e0afb4c42d550e68579bbd72d2496ae
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-10-07 14:21:40 +0200

    CSS edit company

--- Resources/views/partials/dashboard/pagecompany.php
+++ Resources/views/partials/dashboard/pagecompany.php
@@ -1,20 +1,15 @@
-<form method="post" action="/update/company">
-    <input type="hidden" name="id" value="<?= $data['company']['companies'][0]->id  ?>">
-    <div>
-        <label for="name">Name</label>
-        <input type="text" name="name" id="name" value="<?= $data['company']['companies'][0]->Name ?>"  data-default="<?= $data['company']['companies'][0]->Name ?>" disabled>
-    </div>
-    <div>
-        <label for="country">Country</label>
-        <input type="text" name="country" id="country" value="<?= $data['company']['companies'][0]->Country ?>"  data-default="<?= $data['company']['companies'][0]->Name ?>" disabled>
-    </div>
-    <div>
-        <label for="tva">TVA</label>
-        <input type="text" name="tva" id="tva" value="<?= $data['company']['companies'][0]->TVA ?>"  data-default="<?= $data['company']['companies'][0]->Name ?>" disabled>
-    </div>
-    <div>
-        <label for="types"></label>
-        <select name="types" id="types" data-default="<?= $data['company']['companies'][0]->typeid ?>" disabled>
+<div class="dashform__main">
+    <div id="tabs">
+        <form method="post" action="/update/company" class="dashform__form">
+        <input type="hidden" name="id" class="dashform__input" value="<?= $data['company']['companies'][0]->id  ?>">
+        <label for="name" class="dashform__label">Name</label>
+        <input type="text" name="name" class="dashform__input" id="name" value="<?= $data['company']['companies'][0]->Name ?>"  data-default="<?= $data['company']['companies'][0]->Name ?>" disabled>
+        <label for="country" class="dashform__label">Country</label>
+        <input type="text" name="country" class="dashform__input" id="country" value="<?= $data['company']['companies'][0]->Country ?>"  data-default="<?= $data['company']['companies'][0]->Name ?>" disabled>
+        <label for="tva" class="dashform__label">TVA</label>
+        <input type="text" name="tva" id="tva" class="dashform__input" value="<?= $data['company']['companies'][0]->TVA ?>"  data-default="<?= $data['company']['companies'][0]->Name ?>" disabled>
+        <label for="types" class="dashform__label"></label>
+        <select name="types" id="types" class="dashform__input" data-default="<?= $data['company']['companies'][0]->typeid ?>" disabled>
             <?php
             foreach ($data["types"] as $type):
             ?>
@@ -23,13 +18,12 @@
             endforeach;
             ?>
         </select>
+        <button type="submit" id="update" class="dashform__submit" style="display: none">Update</button>
+        <button type="button" id="cancel" class="dashform__submit" style="display: none">Cancel</button>
+        <button type="button" id="edit" class="dashform__submit">Edit</button>
+        </form>
     </div>
-
-
-    <button type="submit" id="update" style="display: none">Update</button>
-    <button type="button" id="cancel" style="display: none">Cancel</button>
-    <button type="button" id="edit">Edit</button>
-</form>
+</div>
 
 <script>
     const update = document.querySelector('#update')
