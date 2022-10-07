8a3358b658781a9c21b7fd15402f4b70149c7b84
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-10-07 10:05:04 +0200

    Style login

--- Resources/views/pages/sign.php
+++ Resources/views/pages/sign.php
@@ -1,6 +1,5 @@
 <?php
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -8,18 +7,19 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
     <title>Document</title>
 </head>
-<body>
-    <main>
-        <h2>Sign Up</h2>
-        <form action="/sign" method="post" >
-            <input type="text" name="fname" placeholder="Name">
-            <input type="text" name="lname" placeholder="Surname">
-            <input type="email" name="email" placeholder="Email">
-            <input type="password" name="password" placeholder="password">
-            <input type="password" name="postPassword" placeholder="repeat password">
-            <button type="submit">Sign Up</button>
+<body class="sign__body">
+    <main class="sign__main">
+        <h1>Sign Up</h1>
+        <form action="/sign" method="post" class="sign__form">
+            <input type="text" name="fname" class="sign__input" placeholder="Name">
+            <input type="text" name="lname" class="sign__input" placeholder="Surname">
+            <input type="email" name="email" class="sign__input" placeholder="Email">
+            <input type="password" name="password" class="sign__input" placeholder="password">
+            <input type="password" name="postPassword" class="sign__input" placeholder="repeat password">
+            <button type="submit" id="sign__submit">Sign Up</button>
         </form>
     </main>
 </body>
